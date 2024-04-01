ff5fbaf4cdef8e4c9f3a61bdefef203af33f7eed
jonathafernandes <jonathafernandes@live.com>
2024-04-01 10:38:13 -0300

    chore: change favicon

--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -64,7 +64,7 @@ export default {
     },
     error: {
       title: "Not Found",
-      notFound: "Either this page is private or doesn't exist.",
+      notFound: "Esta página não existe ou foi removida.",
     },
     folderContent: {
       folder: "Folder",
